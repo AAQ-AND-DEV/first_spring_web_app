16058b8c2ddec358f609917bd2f704bccdbdf0fb
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-05-02 07:11:01 -0700

    * adds th:each iteration example to home.html, with conditional th:unless * adds List to Model in getHomePage() of HomeController

--- src/main/java/com/aaqanddev/jdnd/course1/controller/HomeController.java
+++ src/main/java/com/aaqanddev/jdnd/course1/controller/HomeController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model){
+        model.addAttribute("greetings", List.of("boo", "yah", "no way", "yada yada"));
         model.addAttribute("welcomeMessage", Instant.now().toString());
         return "home";
     }
