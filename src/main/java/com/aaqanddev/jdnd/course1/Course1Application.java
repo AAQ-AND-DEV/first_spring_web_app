aac0aeb370d0acb25f528d8aa461a1aee13f89a5
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-05-02 17:37:25 -0700

    * adds simplehome.html for redirect via post from form on home.html * sets action and method of form * adds conditional thymeleaf logic to display one msg or other depending on firstVisit(which will be set by form action in future) * adds a bean to simulate server setting firstVisit value, accessed via autowire in controller * adds bean to Course1Application.java for String[] greetings, accessed via autowire in Controller

--- src/main/java/com/aaqanddev/jdnd/course1/Course1Application.java
+++ src/main/java/com/aaqanddev/jdnd/course1/Course1Application.java
@@ -2,6 +2,7 @@ package com.aaqanddev.jdnd.course1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Course1Application {
@@ -10,4 +11,13 @@ public class Course1Application {
 		SpringApplication.run(Course1Application.class, args);
 	}
 
+	@Bean
+	public Boolean firstVisit(){
+		return false;
+	}
+
+	@Bean
+	public String[] greetings(){
+		return new String[]{"Hello, homepage!","Welcome back!"};
+	}
 }
--- src/main/java/com/aaqanddev/jdnd/course1/controller/HomeController.java
+++ src/main/java/com/aaqanddev/jdnd/course1/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.aaqanddev.jdnd.course1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.Instant;
@@ -12,10 +14,24 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private Boolean firstVisit;
+
+    @Autowired
+    private String[] greetings;
+
     @RequestMapping("/home")
     public String getHomePage(Model model){
         model.addAttribute("greetings", List.of("boo", "yah", "no way", "yada yada"));
         model.addAttribute("welcomeMessage", Instant.now().toString());
+        model.addAttribute("firstVisit", firstVisit);
+        model.addAttribute("hello", greetings[0]);
+        model.addAttribute("welcomeBack", greetings[1]);
         return "home";
+    }
+
+    @PostMapping("/simplehome")
+    public String getSimpleHomePage(Model model){
+        return "simplehome";
     }
 }
